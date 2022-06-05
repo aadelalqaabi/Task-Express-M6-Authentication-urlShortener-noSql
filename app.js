ca49b35b3711db927425c3d4e3079c0a4a2186f3
aa0241 <77753344+aa0241@users.noreply.github.com>
2022-06-05 18:38:39 +0300

    DONE

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const app = express();
 const urlRoutes = require("./api/urls/urls.routes");
 const userRoutes = require("./api/users/users.routes");
 const passport = require("passport");
-const { localStrategy } = require("./middleware/passport");
+const { localStrategy, jwtStrategy } = require("./middleware/passport");
 
 connectDb();
 app.use(express.json());
 app.use(passport.initialize());
 passport.use(localStrategy);
+passport.use(jwtStrategy);
 app.use("/urls", urlRoutes);
 app.use(userRoutes);
 
--- middleware/passport.js
+++ middleware/passport.js
@@ -1,6 +1,9 @@
-const LocalStrategy = require("passport-local");
+const LocalStrategy = require("passport-local").Strategy;
+const JWTStrategy = require("passport-jwt").Strategy;
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const { JWT_SECRET } = require("../config/keys");
+const { fromAuthHeaderAsBearerToken } = require("passport-jwt/lib/extract_jwt");
 
 exports.localStrategy = new LocalStrategy(async (username, password, done) => {
   try {
@@ -17,3 +20,18 @@ exports.localStrategy = new LocalStrategy(async (username, password, done) => {
     done(error);
   }
 });
+
+exports.jwtStrategy = new JWTStrategy(
+  { jwtFromRequest: fromAuthHeaderAsBearerToken(), secretOrKey: JWT_SECRET },
+  async (jwtPayload, done) => {
+    if (Date.now() > jwtPayload.exp) {
+      return done(null, false);
+    }
+    try {
+      const user = await User.findById(jwtPayload.id);
+      done(null, user);
+    } catch (error) {
+      done(error);
+    }
+  }
+);
